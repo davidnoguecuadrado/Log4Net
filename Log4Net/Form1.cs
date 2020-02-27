141fa60d6391e0162ab3c13e4d94d9121e9172c9
davidnoguecuadrado <davidnogue08@gmail.com>
2020-02-27 10:31:26 +0100

    Agregar archivos de proyecto.

---
+++ Log4Net/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+using log4net;
+
+namespace Log4Net
+{
+    public partial class Form1 : Form
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Form1));
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            log.Info("Load");
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            log.Info("Click");
+        }
+    }
+}
